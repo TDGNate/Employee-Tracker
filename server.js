5c6fc4416b5b502d7f3a5b5a35e40f7f47960f86
Nate <itsnzte@gmail.com>
2022-08-11 08:27:30 -0700

    Added init function in server js file. Added menu feature so users can choose how they want to proceed

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const inquirer = require("inquirer");
+
+const menu = [
+  {
+    type: "list",
+    message: "What's the next move?",
+    choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
+    name: "menu"
+  }
+]
+
+async function init() {
+  let appRunning = true;
+  let userMenuPick = inquirer.prompt(menu).then(data => {
+    console.log(data.menu)
+    return data.menu
+  });
+}
+
+init()
