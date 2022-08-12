56da2b38e50b15f1ad700066984ac98b3bbe48d0
Nate <itsnzte@gmail.com>
2022-08-12 11:39:56 -0700

    Updated connection file, imported mysql2 and created connection to database

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,14 @@
-// // import sequelize and dotenv
-// const sequelize = require('sequelize');
-// require('dotenv').config()
+// import sequelize and dotenv
+const mysql = require('mysql2')
+require('dotenv').config()
 
-// // Create a connection to database
-// const sqlDatabase = new sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: 'localhost',
-//   }
-// )
+// Create a connection to database
+const sqlDatabase = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+  }
+)
+
+module.exports = sqlDatabase;
