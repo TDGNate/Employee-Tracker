1d1beb9403230ea5555fc86cdf10e582d762259d
Nate <itsnzte@gmail.com>
2022-08-11 18:48:20 -0700

    Added exit option to quit application

--- server.js
+++ server.js
@@ -4,17 +4,35 @@ const menu = [
   {
     type: "list",
     message: "What's the next move?",
-    choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
+    choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Exit"],
     name: "menu"
   }
 ]
 
 async function init() {
   let appRunning = true;
-  let userMenuPick = inquirer.prompt(menu).then(data => {
+  let userMenuPick = await inquirer.prompt(menu).then(data => {
     console.log(data.menu)
     return data.menu
   });
+
+  // if the app is still running 
+  while (appRunning) {
+    switch (userMenuPick) {
+      case "View all departments":
+        console.log("view all departments")
+        break;
+      case "View all roles":
+        console.log('View all roles')
+        break;
+      case "View all employees":
+        console.log("View all employees")
+        break;
+      case "Exit":
+        console.log('exit')
+        appRunning = false
+    }
+  }
 }
 
 init()
